545cdef14985a54dd90530c440b3578809bf824e
Amer Hamzeh <amer@amerdid.it>
2016-10-30 15:55:53 +0100

    feat(members): return language stats about a member

--- members.rb
+++ members.rb
@@ -3,6 +3,7 @@
 
 require 'rubygems'
 require 'commander'
+
 require File.expand_path('../fetcher', __FILE__)
 
 class Members
@@ -22,7 +23,40 @@ class Members
       end
     end
 
+    command :list_languages do |c|
+      c.syntax = 'members list_languages'
+      c.description = 'Lists the numbers of repos per language for a given member'
+      c.option '--prefix STRING', String, 'Adds a prefix to bar'
+      c.option '--suffix STRING', String, 'Adds a suffix to bar'
+      c.action do |_args, _options|
+        redis = Redis.new(url: ENV['REDIS_URI'])
+
+        say 'The following members exist in your organisation:'
+        redis.keys.each do |key|
+          say '- ' + key
+        end
+
+        say "\nPlease input the name of the member you are curious about."
+        member_name = ask("\nMember Name: ")
+
+        stats = JSON.parse(redis.get(member_name))
+        languages = sorted_languages stats
+
+        say "\n - #{member_name}"
+        languages.each do |language, count|
+          say "    #{language} - #{count}"
+        end
+      end
+    end
+
     run!
+  end
+
+  def sorted_languages(stats)
+    repos = stats['repos']
+    languages = repos.map { |r| r['language'] }
+    languages = Hash[languages.group_by { |x| x }.map { |k, v| [k, v.count] }]
+    languages.sort_by { |_language, count| count }.reverse
   end
 end
 
