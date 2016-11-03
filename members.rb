36cfd055e28f01f0a5ae486bc1d0652998124a18
Amer Hamzeh <amer@amerdid.it>
2016-11-03 17:09:27 +0100

    refactor: clean up members class

--- members.rb
+++ members.rb
@@ -7,32 +7,54 @@ require 'commander'
 require File.expand_path('../app/fetcher', __FILE__)
 require File.expand_path('../app/stats', __FILE__)
 
+# = Members
+#
+# This class represents the entry point of the command line application. We
+# are using the commander gem in the modular syntax to define the commends.
+# For more info pleaase see: https://github.com/commander-rb/commander
 class Members
   include Commander::Methods
-  # include whatever modules you need
 
   def run
-    program :name, 'members'
-    program :version, '0.0.1'
-    program :description, 'Retrieves language/repo info about members from a Github organisation.'
+    configure_program_metadata
+    define_commands
+    run!
+  end
 
-    command :fetch do |c|
-      c.syntax = 'members fetch'
-      c.summary = 'Populates the database with the members of your organisation.'
-      c.action do |_args, _options|
-        Fetcher.new.fetch
-      end
-    end
+  private
 
+  def define_commands
+    define_fetch
+    define_list_languages
+  end
+
+  def define_list_languages
     command :list_languages do |c|
       c.syntax = 'members list_languages'
-      c.description = 'Lists the numbers of repos per language for a given member'
+      c.description =
+        'Lists the numbers of repos per language for a given member'
       c.action do |_args, _options|
         Stats.list_languages
       end
     end
+  end
 
-    run!
+  def define_fetch
+    command :fetch do |c|
+      c.syntax = 'members fetch'
+      c.summary =
+        'Populates the database with the members of your organisation.'
+      c.action do |_args, _options|
+        Fetcher.new.fetch
+      end
+    end
+  end
+
+  def configure_program_metadata
+    program :name, 'members'
+    program :version, '0.0.1'
+    program :description, 'Retrieves language/repo info about members from a' \
+                          'Github organisation.'
   end
 end
 
